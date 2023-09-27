084a7d2f29cf55179f8493c2fab92269d04fcb7f
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2023-09-27 16:13:42 +0100

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
+  //Arrange
   render(<App />);
+  //Act
   const linkElement = screen.getByText(/learn react/i);
+  //Assert
   expect(linkElement).toBeInTheDocument();
 });
